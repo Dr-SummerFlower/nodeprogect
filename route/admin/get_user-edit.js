df381aa855c3a8fea940aab1b46836f41d059ebc
Dr-SummerFlower <1721807170@qq.com>
2023-04-17 14:30:47 +0800

    modified:   route/admin/get_user-edit.js 	modified:   route/admin/get_user.js 	modified:   views/admin/user-edit.html 	modified:   views/admin/user.html

--- route/admin/get_user-edit.js
+++ route/admin/get_user-edit.js
@@ -1,3 +1,9 @@
-module.exports = (req,res)=>{
-    res.render('admin/user-edit');
+const { User } = require('../../model/user')
+
+module.exports = async (req,res)=>{
+    const id = req.query.id
+    const f = await User.findOne({_id: id})
+    res.render('admin/user-edit', {
+        user: f
+    })
 }
--- route/admin/get_user.js
+++ route/admin/get_user.js
@@ -1,3 +1,8 @@
-module.exports = (req,res)=>{
-    res.render('admin/user');
+const {User} = require('../../model/user')
+
+module.exports = async (req,res)=>{
+    const users = await User.find()
+    res.render('admin/user', {
+        users
+    });
 }
